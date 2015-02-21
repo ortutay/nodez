ac3c9f27abb764e3b36825175cd2173cf21c4949
ortutay <marcell.ortutay@gmail.com>
2015-02-21 15:33:55 -0800

    prune message stream for memory

--- static/js/main.js
+++ static/js/main.js
@@ -15,7 +15,14 @@ function handleWireStreamMessage(e) {
         for (var i = 0; i < d["Inv"].length; i++) {
             item = d["Inv"][i]
             el = $("#messageStream")
-                .prepend("<div/>", item["Hash"]);
+                .prepend("<div class='message-stream-item'>" + item["Hash"] + "</div>");
+        }
+
+        // Prune messages to avoid using too much memory. CSS overflow is used
+        // for styling.
+        var items = $("#messageStream .message-stream-item");
+        for (var i = items.length; i > 500; i--) {
+            items.eq(i).remove();
         }
         break;
     }
