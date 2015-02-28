1b3fbe5e03bde5142eba06abc01029f4816583b2
ortutay <marcell.ortutay@gmail.com>
2015-02-27 23:16:00 -0800

    show sync info

--- static/js/main.js
+++ static/js/main.js
@@ -13,20 +13,22 @@ function handleWireStreamMessage(e) {
     console.log(d)
 
     switch (d["Command"]) {
+    case "sync":
+        item = d["Sync"]
+        el = $("#messageStream").prepend("<div class='message-stream-item'>" + item["Hash"] + "</div>");
+
     case "inv":
         for (var i = 0; i < d["Inv"].length; i++) {
             item = d["Inv"][i]
-            el = $("#messageStream")
-                .prepend("<div class='message-stream-item'>" + item["Hash"] + "</div>");
-        }
-
-        // Prune messages to avoid using too much memory. CSS overflow is used
-        // for styling.
-        var items = $("#messageStream .message-stream-item");
-        for (var i = items.length; i > 500; i--) {
-            items.eq(i).remove();
+            el = $("#messageStream").prepend("<div class='message-stream-item'>" + item["Hash"] + "</div>");
         }
         break;
+    }
+    // Prune messages to avoid using too much memory. CSS overflow is used
+    // for styling.
+    var items = $("#messageStream .message-stream-item");
+    for (var i = items.length; i > 500; i--) {
+        items.eq(i).remove();
     }
 }
 
